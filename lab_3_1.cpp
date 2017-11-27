b2132d0c955cd814900e7ff997ecb2bb8783b6f1
Unknown <Arnon-1251@hotmail.com>
2017-11-27 18:10:35 +0700

    แก้ข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Mr. Arnon Ruangsiri" << endl;
+  cout << "Nicname : Oatty" << endl;
+  cout << "Sex : male" << endl;
+  cout << "Birth : 19/07/1998" << endl;
+  cout << "Hobbies : Read Book and Make cooking" << endl;
+  cout << "Status : Single" << endl;
   return 0;
 }
